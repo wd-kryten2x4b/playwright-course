92d276e681d6300b4c3dd428ec3c9f5fc0ba78f7
Devitt, William <William.Devitt+lyse@lyse.no>
2026-01-29 12:15:21 +0100

    Updated GlobalSetup to be commented out as it is not required for this test

--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -1,26 +1,26 @@
-import { chromium, FullConfig } from "@playwright/test";
+// import { chromium, FullConfig } from "@playwright/test";
 
-async function globalSetup(config: FullConfig)  {
+// async function globalSetup(config: FullConfig)  {
     
-    // new variables for browser & page
-    const browser= await chromium.launch();
-    const page = await browser.newPage()
+//     // new variables for browser & page
+//     const browser= await chromium.launch();
+//     const page = await browser.newPage()
     
-    //open my accounts page
-    await page.goto('https://practice.sdetunicorns.com/my-account');
-     //save Not signed in state to 'loggedInState.json
-    await page.context().storageState({path: 'notLoggedInState.json'});
+//     //open my accounts page
+//     await page.goto('https://practice.sdetunicorns.com/my-account');
+//      //save Not signed in state to 'loggedInState.json
+//     await page.context().storageState({path: 'notLoggedInState.json'});
     
-    //log in
-    await page.locator('#username').fill('practiceuser1');
-    await page.locator('#password').fill('PracticePass1!');
-    await page.locator('[value="Log in"]').click();
+//     //log in
+//     await page.locator('#username').fill('practiceuser1');
+//     await page.locator('#password').fill('PracticePass1!');
+//     await page.locator('[value="Log in"]').click();
 
-    //save signed in state to 'loggedInState.json
-    await page.context().storageState({path: 'loggedInState.json'});
-    //close the browser
-    await browser.close();
-}
+//     //save signed in state to 'loggedInState.json
+//     await page.context().storageState({path: 'loggedInState.json'});
+//     //close the browser
+//     await browser.close();
+// }
     
-export default globalSetup;
+// export default globalSetup;
 
