6f79dce8fd803aa3b72a4a2f4df0911954b703dc
Devitt, William <William.Devitt+lyse@lyse.no>
2026-01-30 09:28:25 +0100

    Updated e2e-yml to change the TestResult folder

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -15,7 +15,7 @@
         this.getStartedBtn = page.locator('#get-started')
         this.headingText = page.locator('text="Think different. Make different."')
         this.homeText = page.locator('#zak-primary-menu:has-text("Home")')
-        this.searchIcon = page.locator('//div[@class="zak-header-actions zak-header-actions--desktop"]//a[@class="zak-header-search_toggle"]')
+        this.searchIcon = page.locator('//div[@class="zak-header-actions zak-header-actions--desktop"]//a[@class="zak-header-search__toggle"]')
         this.navLinks = page.locator('#zak-primary-menu li[id*=menu]')
         this.aboutLink = page.locator('#zak-primary-menu li[id*=menu-item-491]')
     }
